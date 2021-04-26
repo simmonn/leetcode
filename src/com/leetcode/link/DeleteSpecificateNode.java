cd14f0f21a0ac35f4be85246fd49ca9fd4588585
杨胜猛 <yangshengmeng@mininglamp.com>
2021-04-26 23:50:06 +0800

    干掉自己

---
+++ src/com/leetcode/link/DeleteSpecificateNode.java
@@ -0,0 +1,27 @@
+package com.leetcode.link;
+
+/**
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点
+ * 输入：head = [4,5,1,9], node = 5
+ * 输出：[4,1,9]
+ * 解释：给定你链表中值为5的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ *
+ * @author simmon
+ * @since 2021/4/26
+ */
+public class DeleteSpecificateNode {
+
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(4, new ListNode(5, new ListNode(1, new ListNode(9))));
+        deleteNode(listNode);
+        System.out.println(listNode);
+    }
+
+}
