b0ace4b14ab7e7f168cf816cb855c3cdfbf3440f
杨胜猛 <yangshengmeng@mininglamp.com>
2022-04-21 00:56:11 +0800

    peak and tow sum

--- src/com/leetcode/num/TwoSum.java
+++ src/com/leetcode/num/TwoSum.java
@@ -1,6 +1,8 @@
-package com.leetcode;
+package com.leetcode.num;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
@@ -12,6 +14,18 @@ import java.util.Arrays;
  * @since 2021/4/12
  */
 public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+
+    }
 
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
